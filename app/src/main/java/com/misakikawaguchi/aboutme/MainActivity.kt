1e82591f4fe222c94a807a132e078847e08f4ef1
川口美咲 <misaki@arcphilia.co.jp>
2021-01-20 23:00:55 +0900

    [Add]テキスト編集できるようにする

--- app/src/main/java/com/misakikawaguchi/aboutme/MainActivity.kt
+++ app/src/main/java/com/misakikawaguchi/aboutme/MainActivity.kt
@@ -8,6 +8,7 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +18,11 @@ class MainActivity : AppCompatActivity() {
         // ボタンを押す
         findViewById<Button>(R.id.done_button).setOnClickListener {
             addNickname(it)
+        }
+
+        // 名前を表示した箇所をクリックする
+        findViewById<TextView>(R.id.nickname_text).setOnClickListener {
+            updateNickName(it)
         }
     }
 
@@ -41,6 +47,21 @@ class MainActivity : AppCompatActivity() {
         // キーボードを隠す
         val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    // 名前を編集する処理
+    private fun updateNickName(view: View) {
+        // 名前を入力する箇所
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        // ボタン
+        val doneButton = findViewById<Button>(R.id.done_button)
+
+        // 名前を入力した箇所は見えるようにする
+        editText.visibility = View.VISIBLE
+        // ボタンを見えるようにする
+        doneButton.visibility = View.VISIBLE
+        // 表示したテキストは見えないようにする
+        view.visibility = View.GONE
     }
 
 
