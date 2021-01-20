49647d9c559fc0e81203e3dbb799d7ee7cee2c8f
川口美咲 <misaki@arcphilia.co.jp>
2021-01-20 22:40:07 +0900

    [Add]キーボードを隠す処理追加

--- app/src/main/java/com/misakikawaguchi/aboutme/MainActivity.kt
+++ app/src/main/java/com/misakikawaguchi/aboutme/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.misakikawaguchi.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -35,6 +37,10 @@ class MainActivity : AppCompatActivity() {
         view.visibility = View.GONE
         // 名前を表示する箇所を見せる
         nicknameTextView.visibility = View.VISIBLE
+
+        // キーボードを隠す
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
 
 
