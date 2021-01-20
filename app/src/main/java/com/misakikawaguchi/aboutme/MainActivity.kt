3a0396f21168a2ddb810792d2c94a9265ec15ce3
川口美咲 <misaki@arcphilia.co.jp>
2021-01-20 22:32:43 +0900

    [Add]ボタンを押した時の処理追加

--- app/src/main/java/com/misakikawaguchi/aboutme/MainActivity.kt
+++ app/src/main/java/com/misakikawaguchi/aboutme/MainActivity.kt
@@ -2,10 +2,40 @@ package com.misakikawaguchi.aboutme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // ボタンを押す
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            addNickname(it)
+        }
     }
+
+
+    // ボタンを押した時の処理
+    private fun addNickname(view: View) {
+        // 名前を入力する箇所
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        // 名前を表示する箇所
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        // 名前を入力したテキストを名前を表示する箇所に代入する
+        nicknameTextView.text = editText.text
+
+        // 名前を入力した箇所は見えなくする
+        editText.visibility = View.GONE
+        // ボタンを見えなくする
+        view.visibility = View.GONE
+        // 名前を表示する箇所を見せる
+        nicknameTextView.visibility = View.VISIBLE
+    }
+
+
 }
